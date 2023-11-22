f5658497cf94b2a8ce6d8381b86695e04d5c50e3
tauinger-de <45622761+tauinger-de@users.noreply.github.com>
2023-11-22 14:38:44 +0100

    main.go now contains skeleton

--- main.go
+++ main.go
@@ -6,37 +6,19 @@ import (
 )
 
 func main() {
-	leds := [6]machine.Pin{machine.D2, machine.D4, machine.D6, machine.D8, machine.D10, machine.D12}
-	for _, led := range leds {
-		led.Configure(machine.PinConfig{Mode: machine.PinOutput})
-		led.Low()
+	// list and configure all available LED pins
+	ledPins := [6]machine.Pin{machine.D2, machine.D4, machine.D6, machine.D8, machine.D10, machine.D12}
+	for _, pin := range ledPins {
+		pin.Configure(machine.PinConfig{Mode: machine.PinOutput})
+		pin.Low()
 	}
 
+	// setup analog-digital-conversion for analog reading
 	machine.InitADC()
-	tempPin := machine.ADC{machine.ADC5}
+	tempPin := machine.ADC{Pin: machine.ADC5}
 	tempPin.Configure(machine.ADCConfig{})
-	initialTemp := tempPin.Get()
 
+	// main loop
 	for {
-		idx := int((tempPin.Get() - initialTemp) / 500)
-		println(tempPin.Get())
-		for _, led := range leds {
-			led.Low()
-		}
-		if idx >= len(leds) {
-			idx = len(leds) - 1
-		}
-		for n := 0; n <= idx; n++ {
-			leds[n].High()
-		}
-		time.Sleep(time.Millisecond * 500)
 	}
-
-	//for {
-	//	led.Low()
-	//	time.Sleep(time.Millisecond * 500)
-	//
-	//	led.High()
-	//	time.Sleep(time.Millisecond * 500)
-	//}
 }
